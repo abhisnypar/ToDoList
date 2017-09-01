c690280e6ae71fa8a6396f8dbaff0177d31d6793
C_Pidugu,Sai <C_Pidugu.Sai@Cerner.com>
2017-09-01 14:55:33 -0500

    added extra functionalities

--- app/src/main/java/com/cerner/a2do/adapter/ToDoListCursorAdapter.java
+++ app/src/main/java/com/cerner/a2do/adapter/ToDoListCursorAdapter.java
@@ -2,14 +2,18 @@ package com.cerner.a2do.adapter;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CursorAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.cerner.a2do.R;
 import com.cerner.a2do.contentProvider.ToDoListContract;
+import com.cerner.a2do.interfaceListeners.ListItemClickListener;
 
 /**
  * Created by c15333 on 8/31/17.
@@ -19,10 +23,12 @@ public class ToDoListCursorAdapter extends CursorAdapter {
     Context context;
     private LayoutInflater layoutInflater;
     private TextView textView;
+    private ListItemClickListener listItemClickListener;
 
-    public ToDoListCursorAdapter(Context context, Cursor c, int o) {
+    public ToDoListCursorAdapter(Context context, Cursor c, int o, ListItemClickListener listItemClickListener) {
         super(context, c, o);
         this.context = context;
+        this.listItemClickListener = listItemClickListener;
         layoutInflater = LayoutInflater.from(context);
     }
 
@@ -32,11 +38,26 @@ public class ToDoListCursorAdapter extends CursorAdapter {
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, Context context, final Cursor cursor) {
         if (cursor != null) {
             textView = view.findViewById(R.id.list_item_textView);
+            Button button = view.findViewById(R.id.delete_button);
             String taskDescription = cursor.getString(cursor.getColumnIndex(ToDoListContract.TODO_LIST.TASK_DESCRIPTION));
             textView.setText(taskDescription);
+           button.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                   listItemClickListener.onDeleteClicked(view, cursor);
+               }
+           });
         }
+
+        LinearLayout linearLayout = view.findViewById(R.id.list_item_linearlayout);
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listItemClickListener.onCardViewClicked(view, cursor);
+            }
+        });
     }
 }
